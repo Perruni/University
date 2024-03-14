ff352cad631deb8ddd2bb8a706d6ecbfd9cfb8d0
Marcos Malveira <163179361+Perruni@users.noreply.github.com>
2024-03-14 17:56:35 -0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                Person persona = new Person();
+                persona.Name = "Persona " + i;
+                people.Add(persona);
+            }
+
+            foreach (var persona in people)
+            {
+                if (!persona.Name.EndsWith("00"))
+                {
+                    continue;
+                }
+                Console.WriteLine(persona.Name);
+
+            }
+        }
+    }
+}
