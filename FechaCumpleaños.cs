af7eae00b6810698a1fa371f56501bec0d659442
Marcos Malveira <163179361+Perruni@users.noreply.github.com>
2024-03-26 16:48:17 -0300

    Update and rename UnitTest1.cs to FechaCumpleaños.cs

--- FechaCumpleaños.cs
+++ FechaCumpleaños.cs
@@ -12,13 +12,13 @@ namespace Tester
             string mes = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(miNacimiento.Month);
             mes = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(mes);
 
-            string texto = ("Mi cumpleaÒos es el");            
+            string texto = ("Mi cumplea√±os es el");            
 
             string miCumple = $"{texto} {miNacimiento.ToString("dddd dd/MMMM 'de' yyyy")}";
 
             miCumple = miCumple.Replace(miNacimiento.ToString("MMMM"), mes);
 
-            Assert.Equal("Mi cumpleaÒos es el s·bado 01/Diciembre de 2001", miCumple);
+            Assert.Equal("Mi cumplea√±os es el s√°bado 01/Diciembre de 2001", miCumple);
 
         }
     }
